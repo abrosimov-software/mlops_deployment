821b100321478e828fcb0c68f58e3011e28941c1
Daniil Abrosimov <d.abrosimov@innopolis.university>
2024-10-03 23:34:07 +0300

    role back

--- stages/model_engineering/main.py
+++ stages/model_engineering/main.py
@@ -18,10 +18,6 @@ y_train = pd.read_csv("/app/data/preprocessed/y_train.csv")
 X_test = pd.read_csv("/app/data/preprocessed/X_test.csv")
 y_test = pd.read_csv("/app/data/preprocessed/y_test.csv")
 
-# Variables to track the best model
-best_accuracy = -1
-best_run_id = None
-best_model_dir = "/app/models/best_model"
 
 # Function to get all hyperparameter combinations
 def get_hyperparameter_combinations(hyperparameters):
@@ -43,7 +39,7 @@ for experiment in experiments['experiments']:
         
         # Start MLflow run
         run_name = f"{experiment['name']}_{model_name}_run_{i+1}"
-        with mlflow.start_run(run_name=run_name) as current_run:
+        with mlflow.start_run(run_name=run_name):
             
             # Initialize model
             if model_name == 'HistGradientBoostingClassifier':
@@ -60,26 +56,8 @@ for experiment in experiments['experiments']:
                 accuracy = accuracy_score(y_test, y_pred)
                 mlflow.log_metric('accuracy', accuracy)
 
-                # Check if this model has the best accuracy so far
-                if accuracy > best_accuracy:
-                    best_accuracy = accuracy
-                    best_run_id = current_run.info.run_id
-
             # Log hyperparameters and model
             mlflow.log_params(hyperparam_set)
             mlflow.sklearn.log_model(model, model_name)
 
-# Load the best model and save it to the best_model folder
-if best_run_id:
-    print(f"Best model found with accuracy: {best_accuracy}. Run ID: {best_run_id}")
-    
-    # Load the best model from the best_run_id
-    best_model_uri = f"runs:/{best_run_id}/model"
-    best_model = mlflow.sklearn.load_model(best_model_uri)
-
-    # Save the best model to the models/best_model folder
-    mlflow.sklearn.save_model(best_model, best_model_dir)
-    
-    print(f"Best model saved to {best_model_dir}")
-
-print("Experiments and best model selection completed!")
+print("Experiments completed!")
